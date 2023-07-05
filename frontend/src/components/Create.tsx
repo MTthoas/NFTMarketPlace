ca690dc272815de6647c79d26dd34d3a18e48448
Ronfflex <aiglevisnky@gmail.com>
2023-07-05 21:04:27 +0200

    Create NFT front update

--- frontend/src/components/Create.tsx
+++ frontend/src/components/Create.tsx
@@ -118,81 +118,99 @@ function Create() {
   };
 
   return (
-    <div className="w-full">
+    <div className="flex max-w-4xl mx-auto lg:my-10 md:my-8 my-6 lg:px-10 md:px-8 px-6 justify-center">
       {address ? (
-        <div className="px-6 pt-14 lg:px-8">
-          <h1 className="text-center font-bold text-2xl">Create New NFT</h1>
-          <p className="text-center mt-2">Address: {address}</p>
-          <p className="text-center">Balance: {balanceData?.formatted}</p>
-
-          <div className="grid place-items-center p-2 my-6">
-            <div className="text-center place-items-center grid border border-gray-400 p-16 rounded-xl border-dashed">
-              <svg
-                className="w-12 h-12 text-black"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                ></path>
-              </svg>
-              <h1 className="text-grey font-semibold text-2xl">
-                JPG, PNG, GIF. Max 10mb.
-              </h1>
-              <input
-                type="file"
-                onChange={onFileChange}
-                className="block w-full text-sm text-slate-500
-                          file:mr-4 file:py-2 file:px-4
-                          file:rounded-full file:border-0
-                          file:text-sm file:font-semibold
-                          file:bg-black-50 file:text-black-700
-                          hover:file:bg-black-100"
-              />
-            </div>
+        <div>
+          {/* Title */}
+          <div className="">
+            <h1 className="font-bold text-2xl">Create New NFT</h1>
+            <h3 className="">Single edition on Etherum</h3>
           </div>
 
-          <div className="w-100 grid justify-center">
-            <div className="grid my-2 mx-4">
-              <label className="font-bold text-base">Name</label>
-              <input
-                type="text"
-                value={tokenName}
-                onChange={(e) => setTokenName(e.target.value)}
-                placeholder="Mon premier NFT"
-                className="border-2 border-gray-200 p-2 rounded-lg focus:outline-none focus:border-black-500 transition-colors"
-              />
+          {/* Form */}
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {/* Left */}
+            <div className="col-span-1 lg:col-span-2 flex flex-col justify-center">
+
+              {/* Upload file */}
+              <div className="grid place-items-center p-2 my-6">
+                <div className="text-center place-items-center grid border border-gray-400 p-16 rounded-xl border-dashed">
+                  <svg
+                    className="w-12 h-12 text-black"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                    ></path>
+                  </svg>
+                  <h1 className="text-grey font-semibold text-2xl">
+                    JPG, PNG, GIF. Max 10mb.
+                  </h1>
+                  <input
+                    type="file"
+                    onChange={onFileChange}
+                    className="block w-full text-sm text-slate-500
+                            file:mr-4 file:py-2 file:px-4
+                            file:rounded-full file:border-0
+                            file:text-sm file:font-semibold
+                            file:bg-black-50 file:text-black-700
+                            hover:file:bg-black-100"
+                  />
+                </div>
+              </div>
+
+              {/* Inputs */}
+              <div className="grid place-items-center">
+                <div className="grid my-2 mx-4">
+                  <label className="font-bold text-base">Name</label>
+                  <input
+                    type="text"
+                    value={tokenName}
+                    onChange={(e) => setTokenName(e.target.value)}
+                    placeholder="Mon premier NFT"
+                    className="border-2 border-gray-200 p-2 rounded-lg focus:outline-none focus:border-black-500 transition-colors"
+                  />
+                </div>
+                <div className="grid my-2 mx-4">
+                  <label className="font-bold text-base">Price</label>
+                  <input
+                    type="text"
+                    value={tokenPrice}
+                    onChange={(e) => setTokenPrice(e.target.value)}
+                    placeholder="Price in Ether"
+                    className="border-2 border-gray-200 p-2 rounded-lg focus:outline-none focus:border-black-500 transition-colors"
+                  />
+                </div>
+                <div className="grid my-2 mx-4">
+                  <label className="font-bold text-base">Description</label>
+                  <input
+                    type="text"
+                    value={tokenDescription}
+                    onChange={(e) => setTokenDescription(e.target.value)}
+                    placeholder="NFT Description"
+                    className="border-2 border-gray-200 p-2 rounded-lg focus:outline-none focus:border-black-500 transition-colors"
+                  />
+                </div>
+                <button
+                  onClick={createNewNFT}
+                  className="bg-transparent hover:bg-secondary text-neutral font-semibold hover:text-white py-2 px-4 my-4 border border-neutral hover:border-transparent rounded-full"
+                >
+                  Create NFT
+                </button>
+              </div>
             </div>
-            <div className="grid my-2 mx-4">
-              <label className="font-bold text-base">Price</label>
-              <input
-                type="text"
-                value={tokenPrice}
-                onChange={(e) => setTokenPrice(e.target.value)}
-                placeholder="Price in Ether"
-                className="border-2 border-gray-200 p-2 rounded-lg focus:outline-none focus:border-black-500 transition-colors"
-              />
+
+            {/* Right */}
+            <div className="hidden lg:block flex flex-col justify-center">
+              <div className="grid">
+                <p>dqfsf</p>
+              </div>
             </div>
-            <div className="grid my-2 mx-4">
-              <label className="font-bold text-base">Description</label>
-              <input
-                type="text"
-                value={tokenDescription}
-                onChange={(e) => setTokenDescription(e.target.value)}
-                placeholder="NFT Description"
-                className="border-2 border-gray-200 p-2 rounded-lg focus:outline-none focus:border-black-500 transition-colors"
-              />
-            </div>
-            <button
-              onClick={createNewNFT}
-              className="bg-transparent hover:bg-secondary text-neutral font-semibold hover:text-white py-2 px-4 my-4 border border-neutral hover:border-transparent rounded-full"
-            >
-              Create NFT
-            </button>
           </div>
         </div>
       ) : null}
