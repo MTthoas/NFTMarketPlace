568746d11da480c1b3185a0b08c565de2646d5b7
MTthoas <mpecquery@myges.fr>
2023-07-16 22:52:12 +0200

    change port'

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ require('dotenv').config();
 
 
 const app = express();
-const port = process.env.PORT;
+const port = 3030;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
